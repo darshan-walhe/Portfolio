06768b1ddf021491a3aec2c5bb391c3c6283f61c
darshan-walhe <darshanwalhe1510@gmail.com>
2026-09-09 16:57:20 +0530

    update backend URI in frontend

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,5 +1,5 @@
-//const API_URL = 'https://portfolio-ogjb.vercel.app';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://portfolio-ogjb.vercel.app';
+
 
 
 const getAuthHeader = () => {
